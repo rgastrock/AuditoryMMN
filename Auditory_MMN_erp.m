611675fac40f16b9aeb20a61d6a47dfc4acc9e07
Raphael Q. Gastrock <41796119+rgastrock@users.noreply.github.com>
2023-02-24 16:48:47 -0500

    updated code

--- Auditory_MMN_erp.m
+++ Auditory_MMN_erp.m
@@ -3,52 +3,74 @@
 % script to calculate MMN
 
 %% Extract epochs
+eeglab_path = 'F:\Documents\eeglab_current\eeglab2021.1';  % change to where your eeglabfolder is
+erplab_path = 'F:\Documents\eeglab_current';  % change to where your erplabfolder is
+set_path = 'F:\Documents\Science\AuditoryMMN\data\ana_1'; % where you want data files to be saved
+data_path = 'F:\Documents\Science\AuditoryMMN\data'; % where you want to read data from
 
-%read in processed data then:
+% % addpath to eeglab
+addpath(eeglab_path);
+addpath(erplab_path);
+eeglab;
+close all
 
-conditions = {
-    'standard'
-    'oddball'
-    };
-triggers = {'16140' '16141'};
-for cond_idx = 1:length(conditions)
-    data = pop_epoch(EEG, triggers(cond_idx), [-0.2 0.8]);
-    data = pop_rmbase(data, [-200, 0]);
+cd(set_path);
+filenames = dir('*.set'); %looks for all .set files. Will work if you only have 1 .set file for each participant
+participants = 1; %change to 1:x where x is the total number of participants
+
+for pp = 1:length(participants)
+    %read in processed data then:
+    EEG = pop_loadset(sprintf('erp_%d_prepro.set', pp)); 
+    %then you have the regular and deviant conditions (consider 2 for now)
+    conditions = {
+        'deviant_high'
+        'deviant_low'
+        };
+    %pick out events/triggers that match specific conditions
+    %start with Inv1, dev, high, attended (1111)
+    %and with Inv1, reg, high, attended (1211)
+    triggers = {'1111' '1121'};
+    for cond_idx = 1:length(conditions)
+        data = pop_epoch(EEG, triggers(cond_idx), [-0.8 0.8]);
+        data = pop_rmbase(data, [-200, 0]);
     
-    %Save preprocessed
-    fname = sprintf('1_%s.set', conditions{cond_idx});
-    pop_saveset(data, fname, set_path);
+        %Save preprocessed
+        fname = sprintf('erp_%d_%s.set', pp, conditions{cond_idx});
+        pop_saveset(data, fname, set_path);
     
+    end
 end
 
 %% Load and plot
+%currently can only do it for 1 participant. But you'd want a grand average
+%across participants once you have more, and that is what you'll plot
+%below, according to conditions
 cd(set_path)
 close all
-standard = pop_loadset('1_standard.set');
-oddball = pop_loadset('1_oddball.set');
+devh = pop_loadset('erp_1_deviant_high.set');
+devl = pop_loadset('erp_1_deviant_low.set');
 
 % Data format is channels x time points x trials
 get_ERP = @(condition, channels) squeeze(mean(condition.data(channels, :, :), [1 3]));
 
-% Uncomment the channels you want to plot
-%chans = [3, 4, 11]; % Oz, O1, O2
-chans = [2, 5, 8, 9, 10]; % C3, POz, Fz, Cz, C4
+% channels you want to plot - currently guessed electrode locations
+chans = [1,4,5,6,9,11,13, 34, 39, 40, 41, 44, 46, 50]; % Fp1, F1, F3, F5, FC1, FC5, C3; Fp2, F2, F4, F6, FC2, FC6, C4
 
 %% Plot
 figure
-dif_wav = get_ERP(oddball, chans) - get_ERP(standard, chans);
+dif_wav = get_ERP(devh, chans) - get_ERP(devl, chans);
 
-plot(oddball.times, get_ERP(standard, chans), 'b', 'LineWidth', 2)
+plot(devh.times, get_ERP(devh, chans), 'b', 'LineWidth', 2)
 hold on
-plot(oddball.times, get_ERP(oddball, chans), 'r', 'LineWidth', 2)
+plot(devh.times, get_ERP(devl, chans), 'r', 'LineWidth', 2)
 hold on
-plot(oddball.times, dif_wav, 'k', 'LineWidth', 2)
+plot(devh.times, dif_wav, 'k', 'LineWidth', 2)
 
 xline(0)
 xline(300)
 yline(0)
 
-legend('Standard', 'Oddball', 'Difference')
+legend('deviant_high', 'deviant_low', 'Difference')
 legend('boxoff')
 legend('Location', 'northwest')
 set(gca,'FontSize', 13)
--- Auditory_MMN_prepro.m
+++ Auditory_MMN_prepro.m
@@ -5,32 +5,37 @@
 
 %% set up path
 eeglab_path = 'F:\Documents\eeglab_current\eeglab2021.1';  % change to where your eeglabfolder is
+erplab_path = 'F:\Documents\eeglab_current';  % change to where your erplabfolder is
 set_path = 'F:\Documents\Science\AuditoryMMN\data\ana_1'; % where you want data files to be saved
 data_path = 'F:\Documents\Science\AuditoryMMN\data'; % where you want to read data from
 
 % % addpath to eeglab
 addpath(eeglab_path);
+addpath(erplab_path);
 eeglab;
 close all
 
 cd(data_path);
 filenames = dir('*.set'); %looks for all .set files. Will work if you only have 1 .set file for each participant
-participants = 1:5; %change to 1:x where x is the total number of participants
+participants = 1; %change to 1:x where x is the total number of participants
 
 for pp = 1:length(participants)
     
     % Import data (change filenames idx to change file)
     EEG = pop_loadset(sprintf('erp_%d.set', pp)); 
-    % run functions in your history file here, line by line
-    %hard for me to know the parameters you want for every function
-    [EEG, eventnumbers] = pop_importevent(EEG);
-    %pop_continuousartdet
-    %save as new set
-    %pop_runica
-    %etc
-
+    % need to change the parameters for each function, depending on your
+    % needs
+    [EEG, eventnums] = pop_importevent(EEG, 'append','no','event','eventsfeb21.txt','fields',{'number','type','latency','duration'},'skipline',1,'timeunit',0.0039062,'align',0);
+    % car seems to be an erplab extension
+    %EEG = pop_continuousartdet(EEG , 'ampth',  500, 'chanArray',  1:64, 'colorseg', [ 1 0.9765 0.5294], 'forder',  100, 'numChanThreshold',  1, 'stepms',  50, 'threshType', 'peak-to-peak', 'winms',  2000 );
+    EEG = pop_runica(EEG, 'icatype', 'runica', 'extended',1,'interrupt','on');
+    EEG = pop_iclabel(EEG, 'default');
+    EEG = pop_icflag(EEG, [0 0;0.9 1;0.9 1;0.9 1;0.9 1;0.9 1;0 0]);
+    EEG = pop_subcomp( EEG, [], 0);
     %end would be to save a .set file that the other script will use to
     %calculate erps
+    fname = sprintf('erp_%d_prepro.set', pp);
+    pop_saveset(EEG, fname, set_path);
 
 end
 
